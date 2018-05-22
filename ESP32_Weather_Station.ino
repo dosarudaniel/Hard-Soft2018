0b7fdd04c998d061e78b476e2c83a740541ec6b9
Daniel-Florin Dosaru <dosarudaniel@gmail.com>
2018-05-22 11:29:35 +0300

    Removed unused code.

--- ESP32_Weather_Station.ino
+++ ESP32_Weather_Station.ino
@@ -33,15 +33,12 @@ WiFiClient client;
 char* servername ="54.149.135.147";  // remote server we will connect to
 String result;
 
-int  iterations = 1800;
 String weatherDescription ="";
 String weatherLocation = "";
 float Temperature;
 
 void setup() 
 {
-  pinMode(LED_PIN, OUTPUT);
-  
   Serial.begin(9600);
 
   initSensor();
@@ -50,25 +47,12 @@ void setup()
 }
 
 void loop() {
- // if(iterations == 2)//We check for updated weather forecast once every hour
- // {
-   getWeatherData();
-   printWeatherIcon(weatherID);
-   iterations = 0;   
-// }
-
  getTemperature();
- sendTemperatureToNextion();
  
  getHumidity();
- sendHumidityToNextion();
  
  getPressure();
- sendPressureToNextion();
-
- iterations++;
- 
- blinkLED();
+ getWeatherData();
  delay(2000);
 }
 
@@ -93,13 +77,6 @@ void initSensor()
     Serial.println("Could not find a valid BME280 sensor, check wiring!");
     while (1);
   }
-}
-
-void blinkLED()
-{
-  digitalWrite(LED_PIN,HIGH);
-  delay(100);
-  digitalWrite(LED_PIN,LOW);
 }
 
 float getTemperature()
@@ -172,197 +149,5 @@ String idString = root["list"]["weather"]["id"];
 String timeS = root["list"]["dt_txt"];
 
 weatherID = idString.toInt();
-Serial.print("\nWeatherID: ");
-Serial.print(weatherID);
-endNextionCommand(); //We need that in order the nextion to recognise the first command after the serial print
-
+Serial.println (url);
 }
-
-void showConnectingIcon()
-{
-  Serial.println();
-  String command = "weatherIcon.pic=3";
-  Serial.print(command);
-  endNextionCommand();
-}
-
-void sendHumidityToNextion()
-{
-  String command = "humidity.txt=\""+String(humidity,1)+"\"";
-  Serial.print(command);
-  endNextionCommand();
-}
-
-void sendTemperatureToNextion()
-{
-  String command = "temperature.txt=\""+String(temperature,1)+"\"";
-  Serial.print(command);
-  endNextionCommand();
-}
-
-void sendPressureToNextion()
-{
-  String command = "pressure.txt=\""+String(pressure,1)+"\"";
-  Serial.print(command);
-  endNextionCommand();
-}
-
-void endNextionCommand()
-{
-  Serial.write(0xff);
-  Serial.write(0xff);
-  Serial.write(0xff);
-}
-
-void printWeatherIcon(int id)
-{
- switch(id)
- {
-  case 800: drawClearWeather(); break;
-  case 801: drawFewClouds(); break;
-  case 802: drawFewClouds(); break;
-  case 803: drawCloud(); break;
-  case 804: drawCloud(); break;
-  
-  case 200: drawThunderstorm(); break;
-  case 201: drawThunderstorm(); break;
-  case 202: drawThunderstorm(); break;
-  case 210: drawThunderstorm(); break;
-  case 211: drawThunderstorm(); break;
-  case 212: drawThunderstorm(); break;
-  case 221: drawThunderstorm(); break;
-  case 230: drawThunderstorm(); break;
-  case 231: drawThunderstorm(); break;
-  case 232: drawThunderstorm(); break;
-
-  case 300: drawLightRain(); break;
-  case 301: drawLightRain(); break;
-  case 302: drawLightRain(); break;
-  case 310: drawLightRain(); break;
-  case 311: drawLightRain(); break;
-  case 312: drawLightRain(); break;
-  case 313: drawLightRain(); break;
-  case 314: drawLightRain(); break;
-  case 321: drawLightRain(); break;
-
-  case 500: drawLightRainWithSunOrMoon(); break;
-  case 501: drawLightRainWithSunOrMoon(); break;
-  case 502: drawLightRainWithSunOrMoon(); break;
-  case 503: drawLightRainWithSunOrMoon(); break;
-  case 504: drawLightRainWithSunOrMoon(); break;
-  case 511: drawLightRain(); break;
-  case 520: drawModerateRain(); break;
-  case 521: drawModerateRain(); break;
-  case 522: drawHeavyRain(); break;
-  case 531: drawHeavyRain(); break;
-
-  case 600: drawLightSnowfall(); break;
-  case 601: drawModerateSnowfall(); break;
-  case 602: drawHeavySnowfall(); break;
-  case 611: drawLightSnowfall(); break;
-  case 612: drawLightSnowfall(); break;
-  case 615: drawLightSnowfall(); break;
-  case 616: drawLightSnowfall(); break;
-  case 620: drawLightSnowfall(); break;
-  case 621: drawModerateSnowfall(); break;
-  case 622: drawHeavySnowfall(); break;
-
-  case 701: drawFog(); break;
-  case 711: drawFog(); break;
-  case 721: drawFog(); break;
-  case 731: drawFog(); break;
-  case 741: drawFog(); break;
-  case 751: drawFog(); break;
-  case 761: drawFog(); break;
-  case 762: drawFog(); break;
-  case 771: drawFog(); break;
-  case 781: drawFog(); break;
-
-  default:break; 
- }
-}
-
-void drawFog()
-{
-  String command = "weatherIcon.pic=13";
-  Serial.print(command);
-  endNextionCommand();
-}
-
-void drawHeavySnowfall()
-{
-  String command = "weatherIcon.pic=8";
-  Serial.print(command);
-  endNextionCommand();
-}
-
-void drawModerateSnowfall()
-{
-  String command = "weatherIcon.pic=8";
-  Serial.print(command);
-  endNextionCommand();
-}
-
-void drawLightSnowfall()
-{
-  String command = "weatherIcon.pic=11";
-  Serial.print(command);
-  endNextionCommand();
-}
-
-void drawHeavyRain()
-{
-  String command = "weatherIcon.pic=10";
-  Serial.print(command);
-  endNextionCommand();
-}
-
-void drawModerateRain()
-{
-  String command = "weatherIcon.pic=6";
-  Serial.print(command);
-  endNextionCommand();
-}
-
-void drawLightRain()
-{
-  String command = "weatherIcon.pic=6";
-  Serial.print(command);
-  endNextionCommand();
-}
-
-void drawLightRainWithSunOrMoon()
-{
-  String command = "weatherIcon.pic=7";
-  Serial.print(command);
-  endNextionCommand(); 
-}
-void drawThunderstorm()
-{
-  String command = "weatherIcon.pic=3";
-  Serial.print(command);
-  endNextionCommand();
-}
-
-void drawClearWeather()
-{
-  String command = "weatherIcon.pic=4";
-  Serial.print(command);
-  endNextionCommand();
-}
-
-void drawCloud()
-{
-  String command = "weatherIcon.pic=9";
-  Serial.print(command);
-  endNextionCommand();
-}
-
-void drawFewClouds()
-{
-  String command = "weatherIcon.pic=5";
-  Serial.print(command);
-  endNextionCommand(); 
-}
-
-
